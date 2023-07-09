3a32a7407a8652d64d18b8f87059b85353c34de1
Marcel <71490170+mnblski@users.noreply.github.com>
2023-07-09 11:11:04 +0100

    add client origin env var to cors config

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_ORIGIN, // CLIENT URL
     credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
