a0951cb333e4b1f66241a54f5f5f02ecc9e05bf8
Marcel <71490170+mnblski@users.noreply.github.com>
2023-07-11 22:09:14 +0100

    add user patch and delete routes in user service. Also rewrite async await with then and catch syntax

--- services/user_services/router.js
+++ services/user_services/router.js
@@ -6,111 +6,104 @@ const { db } = require("../../lib/firebase/main");
 const { fail, sendSuccess } = require("../../utils/res.utils");
 const { verifyToken } = require("../../middleware/auth.middleware");
 const { validate } = require("../../middleware/validation.middleware");
-const { postUserSchema } = require('./schemas');
+const { postUserSchema, putUserSchema } = require('./schemas');
 
 const router = express.Router();
 
 /** checks if the username is taken */
 router.head("/:username", async (req, res, next) => {
-    try {
-        const { username } = req.params;
-        const collectionRef = db.collection('users');
-        const snapshot = await collectionRef.where("username_lc", "==", username.toLowerCase()).get();
+    const { username } = req.params;
+
+    /** also check prev usernames */
+    db.collection('users')
+    .where("username_lc", "==", username.toLowerCase())
+    .get()
+    .then((snapshot) => {
         if (!snapshot.empty) {
-            console.log('test');
             fail("Username is taken", 401);
         }
         return sendSuccess(res)
-    } catch (err) {
-        console.log('err', err);
+    }).catch((err) => {
         next(err)
-    }
+    })
 })
 
-router.get("/:uid", verifyToken, async (req, res, next) => {
-    try {
-        const { uid } = req.params;
-        const snapshot = await db.collection("users").doc(uid).get();
+router.get("/", verifyToken, async (req, res, next) => {
+    db.collection("users").doc(req.uid)
+    .get()
+    .then((snapshot) => {
         if (!snapshot.exists) {
             fail("User not found", 401);
         }
         return sendSuccess(res, snapshot.data())
-    } catch (err) {
+    })
+    .catch((err) => {
         next(err)
-    }
+    })
 })
 
 router.post("/", verifyToken, validate(postUserSchema), async (req, res, next) => {
     const { email, username } = req.body;
-    const docRef = db.collection('users').doc(req.uid);
-    const userData = {
+
+    const data = {
         username,
         username_lc: username.toLowerCase(),
         primary_email: email,
-        created: FieldValue.serverTimestamp(),
+        fc_email: `${username.toLowerCase()}@${process.env.FC_EMAIL_DOMAIN}`,
+        created_at: FieldValue.serverTimestamp(),
     }
-    try {
-        await docRef.set(userData);
-        return sendSuccess(res, userData)
-    } catch (err) {
+
+    db.collection('users').doc(req.uid)
+    .set(data)
+    .then(() => {
+        return sendSuccess(res)
+    })
+    .catch((err) => {
         next(err)
-    }
+    });
 })
 
-// router.get("/health", verifyToken, async (req, res) => {
-//     console.log('health check request:', req);
-//     return sendSuccess(res, { message: "User service is healthy" })
-// })
+router.patch("/", verifyToken, validate(putUserSchema), async (req, res, next) => {
+    const { email, username, ...rest } = req.body;
 
-// router.patch("/:id", validate(userPatchSchema), async (req, res, next) => {
-//     const { id } = req.params;
+    let data = {
+        ...rest,
+        updated_at: FieldValue.serverTimestamp()
+    };
+    if (username) {
+        /**
+         * store previously used in separete collection/table
+         */
+        data.username = username;
+        data.username_lc = username.toLowerCase(),
+        /** this below should be an array
+         * what if user changes the username? what happens to the old email? will the mail be redirected to the new email?s 
+         */
+        data.fc_email = `${username.toLowerCase()}@${process.env.FC_EMAIL_DOMAIN}`;
+    }
+    if (email) {
+       data.primary_email = email;
+    }
 
-//     try {
-//         const docRef = db.collection("users").doc(id);
-//         const snapshot = await docRef.get();
+    db.collection('users').doc(req.uid)
+    .update(data)
+    .then(() => {
+        return sendSuccess(res)
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
 
-//         if (!snapshot.exists) {
-//             fail("User not found");
-//         }
-
-//         const { changes } = req.body;
-
-//         try {
-//             const updatedUser = await docRef.ref.update({
-//                 ...changes,
-//                 updated: FieldValue.serverTimestamp()
-//             });
-
-//             return sendSuccess(res, updatedUser)
-//         } catch (err) {
-//             fail("Error updating the user");
-//         }
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// router.delete("/:id", async (req, res, next) => {
-//     const { id } = req.params;
-
-//     try {
-//         const docRef = db.collection("users").doc(id);
-//         const snapshot = await docRef.get();
-
-//         if (!snapshot.exists) {
-//             fail("User not found");
-//         }
-        
-//         try {
-//             await docRef.delete();
-//             return sendSuccess(res)
-
-//         } catch (err) {
-//             fail("Error deleting the user");
-//         }
-//     } catch (err) {
-//         next(err)
-//     }
-// })
+router.delete("/", verifyToken, async (req, res, next) => {
+    db.collection('users').doc(req.uid)
+    .delete()
+    .then(() => {
+        return sendSuccess(res)
+    })
+    .catch((err) => {
+        next(err)
+    });
+})
 
 module.exports = router;
